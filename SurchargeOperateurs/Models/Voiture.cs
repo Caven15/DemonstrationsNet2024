d511164a85f6e92ceceb745d466c6432b0e4ac2d
Caven <cavenditch15@gmail.com>
2025-01-10 16:03:51 +0100

    Surcharge opérateurs

---
+++ SurchargeOperateurs/Models/Voiture.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SurcharcheOperateur.Models
+{
+    class Voiture
+    {
+        #region Attributs / variables Membres
+
+        private string _Marque;
+        private double _Consommation; // Litres
+        private string[] _Passager = new string[5];
+        public string Modele { get; set; }
+        public int Annee { get; set; } 
+
+        #endregion
+
+        #region Prop's
+
+        public string Marque
+        {
+            get { return _Marque; }
+            set { _Marque = value; }
+        }
+
+        public double Consommation
+        {
+            get { return _Consommation; }
+            set
+            {
+                // Validation : la consomation doit être positive
+                if (value > 0)
+                {
+                    _Consommation = value;
+                }
+                else
+                {
+                    Console.WriteLine("Erreur : La consommation doit être positive !");
+                }
+            }
+        }
+
+        public string this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < _Passager.Length)
+                {
+                    return _Passager[index];
+                }
+                Console.WriteLine("Index hors limites");
+                return null;
+            }
+            set
+            {
+                if (index >= 0 && index < _Passager.Length)
+                {
+                    _Passager[index] = value;
+                }
+                else
+                {
+                    Console.WriteLine("Index hors limites");
+                }
+            }
+        }
+
+        #endregion
+
+        #region Méthodes
+
+        public static Voiture operator +(Voiture v1, Voiture v2)
+        {
+            Voiture NouvelleVoiture = new Voiture()
+            {
+                Marque = v1.Marque,
+                Modele = v2.Modele,
+                Annee = v2.Annee,
+                Consommation = (v1.Consommation + v2.Consommation) / 2 // Moyenne de consommations
+            };
+
+            // Gérer les passager
+
+            return NouvelleVoiture;
+        }
+
+        // Calcul du cout du carburant sur une distance donnée
+        public double CalculCoutCarburant(double distance, double prixCarburant)
+        {
+            return (distance * _Consommation / 100) * prixCarburant;
+        }
+
+        public string AffficherInfos()
+        {
+            return $"Marque : {_Marque}, Modèle : {Modele}, Année : {Annee}, Cosommation : {_Consommation} L/100Km";
+        }
+
+        #endregion
+    }
+}
---
+++ SurchargeOperateurs/Program.cs
@@ -0,0 +1,49 @@
+﻿using SurcharcheOperateur.Models;
+
+Voiture maVoiture = new Voiture();
+maVoiture.Marque = "Honda";
+maVoiture.Modele = "Civic";
+maVoiture.Annee = 2006;
+maVoiture.Consommation = 5.9;
+maVoiture[0] = "Toto";
+maVoiture[1] = "Tata";
+maVoiture[2] = "Titi";
+
+Voiture maVoiture2 = new Voiture();
+maVoiture2.Marque = "Tesla";
+maVoiture2.Modele = "3";
+maVoiture2.Annee = 2022;
+maVoiture2.Consommation = 0.2;
+maVoiture2[0] = "John";
+maVoiture2[1] = "Alain";
+maVoiture2[2] = "Senna";
+
+Voiture VoitureCombine = maVoiture + maVoiture2;
+
+Console.WriteLine(VoitureCombine);
+
+// Initialisation de nos attributs
+
+// Appel de l'indexeur pour récupérer un élément sur base de son index dans le tableau
+
+// Afficher les infos
+Console.WriteLine(VoitureCombine.AffficherInfos());
+
+double distance = 500;      // en km
+double prixCarburant = 1.6; // en €/Litre
+
+double cout = VoitureCombine.CalculCoutCarburant(distance, prixCarburant);
+
+Console.WriteLine("Passagers : ");
+
+for (int i = 1; i <= 3; i++)
+{
+    if (VoitureCombine[i - 1] != null)
+    {
+        Console.WriteLine($"Passager {i} : {VoitureCombine[i - 1]}");
+    }
+}
+
+Console.WriteLine($"Coût du carburant pour {distance} km : {cout} Euros.");
+
+Console.WriteLine(VoitureCombine[0] = "Toto");
