a1b06be4be1f5a49547f5d51833f01f50a97054c
Caven <cavenditch15@gmail.com>
2025-01-10 14:23:00 +0100

    Indexeurs

---
+++ IIndexeurs/Models/Voiture.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoClassProp.Models
+{
+    class Voiture
+    {
+        #region Attributs / variables Membres
+
+        private string _Marque;
+        private double _Consommation; // Litres
+        private string[] _Passager = new string[5];
+        public string Modele { get; set; }
+        public int Annee { get; set; } 
+
+        #endregion
+
+        #region Prop's
+
+        public string Marque
+        {
+            get { return _Marque; }
+            set { _Marque = value; }
+        }
+
+        public double Consommation
+        {
+            get { return _Consommation; }
+            set
+            {
+                // Validation : la consomation doit être positive
+                if (value > 0)
+                {
+                    _Consommation = value;
+                }
+                else
+                {
+                    Console.WriteLine("Erreur : La consommation doit être positive !");
+                }
+            }
+        }
+
+        public string this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < _Passager.Length)
+                {
+                    return _Passager[index];
+                }
+                Console.WriteLine("Index hors limites");
+                return null;
+            }
+            set
+            {
+                if (index >= 0 && index < _Passager.Length)
+                {
+                    _Passager[index] = value;
+                }
+                else
+                {
+                    Console.WriteLine("Index hors limites");
+                }
+            }
+        }
+
+        #endregion
+
+        #region Méthodes
+
+        // Calcul du cout du carburant sur une distance donnée
+        public double CalculCoutCarburant(double distance, double prixCarburant)
+        {
+            return (distance * _Consommation / 100) * prixCarburant;
+        }
+
+        public string AffficherInfos()
+        {
+            return $"Marque : {_Marque}, Modèle : {Modele}, Année : {Annee}, Cosommation : {_Consommation} L/100Km";
+        }
+
+        #endregion
+    }
+}
---
+++ IIndexeurs/Program.cs
@@ -0,0 +1,36 @@
+﻿using DemoClassProp.Models;
+
+Voiture maVoiture = new Voiture();
+
+// Initialisation de nos attributs
+maVoiture.Marque = "Honda";
+maVoiture.Modele = "Civic";
+maVoiture.Annee = 2006;
+maVoiture.Consommation = 5.9;
+
+// Appel de l'indexeur pour récupérer un élément sur base de son index dans le tableau
+maVoiture[0] = "Toto";
+maVoiture[1] = "Tata";
+maVoiture[2] = "Titi";
+
+// Afficher les infos
+Console.WriteLine(maVoiture.AffficherInfos());
+
+double distance = 500;      // en km
+double prixCarburant = 1.6; // en €/Litre
+
+double cout = maVoiture.CalculCoutCarburant(distance, prixCarburant);
+
+Console.WriteLine("Passagers : ");
+
+for (int i = 1; i <= 3; i++)
+{
+    if (maVoiture[i-1] != null)
+    {
+        Console.WriteLine($"Passager {i} : {maVoiture[i-1]}");
+    }
+}
+
+Console.WriteLine($"Coût du carburant pour {distance} km : {cout} Euros.");
+
+Console.WriteLine(maVoiture[0] = "Toto");
