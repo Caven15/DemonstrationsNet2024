0a1a3d802d9de25b888ae299cb5df9596c6d56da
Caven <Caven@DESKTOP-A1FV7EC>
2025-01-10 11:22:35 +0100

    Ajoutez des fichiers projet.

---
+++ DemoClassProp/Models/Voiture.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoClassProp.Models
+{
+    class Voiture
+    {
+        #region Attributs / variables Membres
+
+        private string _Marque;
+        private double _Consommation; // Litres
+
+        public string Modele { get; set; }
+        public int Annee { get; set; } 
+
+        #endregion
+
+        #region Prop's
+
+        public string Marque
+        {
+            get { return _Marque; }
+            set { _Marque = value; }
+        }
+
+        public double Consommation
+        {
+            get { return _Consommation; }
+            set
+            {
+                // Validation : la consomation doit être positive
+                if (value > 0)
+                {
+                    _Consommation = value;
+                }
+                else
+                {
+                    Console.WriteLine("Erreur : La consommation doit être positive !");
+                }
+            }
+        }
+
+        #endregion
+
+        #region Méthodes
+
+        // Calcul du cout du carburant sur une distance donnée
+        public double CalculCoutCarburant(double distance, double prixCarburant)
+        {
+            return (distance * _Consommation / 100) * prixCarburant;
+        }
+
+        public string AffficherInfos()
+        {
+            return $"Marque : {_Marque}, Modèle : {Modele}, Année : {Annee}, Cosommation : {_Consommation} L/100Km";
+        }
+
+        #endregion
+    }
+}
---
+++ DemoClassProp/Program.cs
@@ -0,0 +1,19 @@
+﻿using DemoClassProp.Models;
+
+Voiture maVoiture = new Voiture();
+
+// Initialisation de nos attributs
+maVoiture.Marque = "Honda";
+maVoiture.Modele = "Civic";
+maVoiture.Annee = 2006;
+maVoiture.Consommation = 5.9;
+
+// Afficher les infos
+Console.WriteLine(maVoiture.AffficherInfos());
+
+double distance = 500;      // en km
+double prixCarburant = 1.6; // en €/Litre
+
+double cout = maVoiture.CalculCoutCarburant(distance, prixCarburant);
+
+Console.WriteLine($"Coût du carburant pour {distance} km : {cout} Euros.");
